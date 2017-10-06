815d42fdb6c0baa82d743d5173485fb3ee0124d7
Dmitry V. Krivenok <krivenok.dmitry@gmail.com>
2017-10-06 13:13:51 +0300

    Copy stuff to $HOME from init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Install vim-plug
+echo "Copying everything to $HOME ..."
+cp -rv home/.[a-zA-Z]* $HOME/
+
 echo "Installing vim-plug ..."
 curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || echo "ERROR: vim-plug installation failed"
