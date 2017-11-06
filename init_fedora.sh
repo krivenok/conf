ce532ee2693dbc206690378cb5ca8d0dd4e857fa
Dmitry V. Krivenok <krivenok.dmitry@gmail.com>
2017-11-06 19:43:49 +0300

    added one more Kbuild dependency

--- init_fedora.sh
+++ init_fedora.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum update -y && \
-yum install -y git vim make gcc ctags cscope curl wget tmux tcpdump strace golang chromium bc m4 openssl-devel vim-X11
+yum install -y git vim make gcc ctags cscope curl wget tmux tcpdump strace golang chromium bc m4 openssl-devel vim-X11 elfutils-libelf-devel
