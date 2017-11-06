5156bbda6f0a5c36f40c012b77f42c7db1a60ef9
Dmitry Krivenok <krivenok.dmitry@gmail.com>
2017-11-07 00:35:30 +0300

    Added dependencies for bcc

--- init_fedora.sh
+++ init_fedora.sh
@@ -6,4 +6,8 @@ yum install -y git tig vim make gcc ctags cscope \
 	       golang chromium bc m4 openssl-devel \
 	       vim-X11 elfutils-libelf-devel \
 	       autoconf automake binutils-devel libuuid-devel \
-	       help2man gnu-efi-devel mutt
+	       help2man gnu-efi-devel mutt cmake clang llvm llvm-devel llvm-libs \
+	       iperf bison flex clang-libs clang-devel llvm-static ncurses-devel \
+	       ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
+
+sudo pip install pyroute2
