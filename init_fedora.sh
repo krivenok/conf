fdf00b55fb99e27642db29c775546a04b7ee583e
Dmitry Krivenok <krivenok.dmitry@gmail.com>
2017-11-06 23:25:12 +0300

    Added more fedora packages

--- init_fedora.sh
+++ init_fedora.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 yum update -y && \
-yum install -y git vim make gcc ctags cscope curl wget tmux tcpdump strace golang chromium bc m4 openssl-devel vim-X11 elfutils-libelf-devel
+yum install -y git tig vim make gcc ctags cscope \
+               curl wget tmux tcpdump strace \
+	       golang chromium bc m4 openssl-devel \
+	       vim-X11 elfutils-libelf-devel \
+	       autoconf automake binutils-devel libuuid-devel \
+	       help2man gnu-efi-devel mutt
