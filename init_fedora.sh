a659b59888ab2abfb0ef6b00f058e91a50b34b55
Dmitry Krivenok <krivenok.dmitry@gmail.com>
2017-11-07 17:13:30 +0300

    updated init_fedora.sh with xterm rpm

--- init_fedora.sh
+++ init_fedora.sh
@@ -8,6 +8,7 @@ yum install -y git tig vim make gcc ctags cscope \
 	       autoconf automake binutils-devel libuuid-devel \
 	       help2man gnu-efi-devel mutt cmake clang llvm llvm-devel llvm-libs \
 	       iperf bison flex clang-libs clang-devel llvm-static ncurses-devel \
-	       ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
+	       xterm \
+	       http://mirror.team-cymru.org/rpmforge/redhat/el6/en/x86_64/rpmforge/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
 
 sudo pip install pyroute2
