1bf31ce33f344049a3db2ad24a4e1a5bc38acb2b
Dmitry Krivenok <krivenok.dmitry@gmail.com>
2017-11-09 00:45:39 +0300

    add dnf system-upgrade plugin for dnf

--- init_fedora.sh
+++ init_fedora.sh
@@ -8,7 +8,7 @@ yum install -y git tig vim make gcc ctags cscope \
 	       autoconf automake binutils-devel libuuid-devel \
 	       help2man gnu-efi-devel mutt cmake clang llvm llvm-devel llvm-libs \
 	       iperf bison flex clang-libs clang-devel llvm-static ncurses-devel \
-	       xterm \
+	       xterm 'dnf-command(system-upgrade)' \
 	       http://mirror.team-cymru.org/rpmforge/redhat/el6/en/x86_64/rpmforge/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
 
 sudo pip install pyroute2
