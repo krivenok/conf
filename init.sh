7d09f2104264b1498dc895416f3a872f60727480
Dmitry Krivenok <krivenok.dmitry@gmail.com>
2017-11-09 00:01:24 +0300

    tmux: use tpm and some plugins for tmux

--- init.sh
+++ init.sh
@@ -7,3 +7,8 @@ cp -rv home/.[a-zA-Z]* $HOME/
 echo "Installing vim-plug ..."
 curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || echo "ERROR: vim-plug installation failed"
+
+if [ ! -d "${HOME}/.tmux/plugins/tpm" ] ; then
+	echo "Installing tmux plugin manager ..."
+	git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+fi
