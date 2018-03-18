67396e741a6e264391b384d1fba9a9a2e2c096a4
Dmitry Krivenok <krivenok.dmitry@gmail.com>
2018-03-18 10:22:50 +0100

    added more tools for fedora

--- init_fedora.sh
+++ init_fedora.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 yum update -y && \
 yum install -y git tig vim make gcc ctags cscope \
                curl wget tmux tcpdump strace \
@@ -8,6 +9,8 @@ yum install -y git tig vim make gcc ctags cscope \
 	       autoconf automake binutils-devel libuuid-devel \
 	       help2man gnu-efi-devel mutt cmake clang llvm llvm-devel llvm-libs \
 	       iperf bison flex clang-libs clang-devel llvm-static ncurses-devel \
+	       libcap-devel libidn2-devel nettle-devel \
+	       perltidy perl-Perl-Critic.noarch git-email diffstat \
 	       xterm 'dnf-command(system-upgrade)' \
 	       http://mirror.team-cymru.org/rpmforge/redhat/el6/en/x86_64/rpmforge/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
 
