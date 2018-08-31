e99861b05cf6e31278e7c08e1411fb9ec70f534e
Dmitry Krivenok <krivenok.dmitry@gmail.com>
2018-08-31 23:31:40 +0300

    add vscode

--- init_fedora.sh
+++ init_fedora.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# For golang
 rpm --import https://mirror.go-repo.io/fedora/RPM-GPG-KEY-GO-REPO
 curl -s https://mirror.go-repo.io/fedora/go-repo.repo | tee /etc/yum.repos.d/go-repo.repo
+
+# For vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 dnf update -y && \
 dnf install -y git tig vim make gcc ctags cscope \
@@ -13,7 +18,7 @@ dnf install -y git tig vim make gcc ctags cscope \
 	       iperf bison flex clang-libs clang-devel llvm-static ncurses-devel \
 	       libcap-devel libidn2-devel nettle-devel \
 	       perltidy perl-Perl-Critic.noarch git-email diffstat \
-	       pandoc lynx docker golang \
+	       pandoc lynx docker golang code \
 	       xterm 'dnf-command(system-upgrade)' \
 	       http://ftp.tu-chemnitz.de/pub/linux/dag/redhat/el6/en/x86_64/rpmforge/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
 
